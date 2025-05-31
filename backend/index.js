f928888c3ab477d28d70c372e68624b73741c5fc
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-05-31 02:21:02 +0100

    first-commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/authRoute.js"
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+app.use("/api/auth", authRoutes)
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port`, PORT)
+})
